5c7a6a41bdcd9038ae35a05b4cf28cda06449be8
Alex Saskevich <alexhomel@ya.ru>
2014-07-05 21:56:13 +0300

    StackOverflow fixed

--- validator.go
+++ validator.go
@@ -121,7 +121,7 @@ func IsCreditCard(str string) bool {
 		if shouldDouble {
 			tmpNum *= 2
 			if tmpNum >= 10 {
-				sum += ((tmpNum%10)+1)
+				sum += ((tmpNum % 10) + 1)
 			} else {
 				sum += tmpNum
 			}
@@ -149,30 +149,31 @@ func IsISBN(str string, version int) bool {
 			return false
 		}
 		for i = 0; i < 9; i++ {
-			checksum += (i+1)*int32(sanitized[i] - '0')
+			checksum += (i + 1) * int32(sanitized[i]-'0')
 		}
 		if sanitized[9] == 'X' {
-			checksum += 10*10
+			checksum += 10 * 10
 		} else {
-			checksum += 10*int32(sanitized[9] - '0')
+			checksum += 10 * int32(sanitized[9]-'0')
 		}
 		if checksum%11 == 0 {
 			return true
 		}
-	}
-	if version == 13 {
+	} else if version == 13 {
 		if !Matches(string(sanitized), ISBN13) {
 			return false
 		}
 		factor := []int32{1, 3}
 		for i = 0; i < 12; i++ {
-			checksum += factor[i%2]*int32(sanitized[i] - '0')
+			checksum += factor[i%2] * int32(sanitized[i]-'0')
 		}
-		if (int32(sanitized[12] - '0'))-((10-(checksum%10))%10) == 0 {
+		if (int32(sanitized[12]-'0'))-((10-(checksum%10))%10) == 0 {
 			return true
 		}
+	} else {
+		return IsISBN(str, 10) || IsISBN(str, 13)
 	}
-	return IsISBN(str, 10) || IsISBN(str, 13)
+	return false
 }
 
 // IsJSON check if the string is valid JSON (note: uses json.Unmarshal).
@@ -220,7 +221,7 @@ func IsIP(str string, version int) bool {
 		}
 		parts := strings.Split(str, ".")
 		isIPv4 := true
-		for i := 0; i < len(parts); i ++ {
+		for i := 0; i < len(parts); i++ {
 			part_i, _ := ToInt(parts[i])
 			isIPv4 = isIPv4 && ((part_i >= 0) && (part_i <= 255))
 		}
--- validator_test.go
+++ validator_test.go
@@ -358,12 +358,12 @@ func Test_IsISBN(t *testing.T) {
 func Test_IsBase64(t *testing.T) {
 	tests := []string{"TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2NpbmcgZWxpdC4=",
 		"Vml2YW11cyBmZXJtZW50dW0gc2VtcGVyIHBvcnRhLg==", "U3VzcGVuZGlzc2UgbGVjdHVzIGxlbw==",
-								"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuMPNS1Ufof9EW/M98FNw" +
-										"UAKrwflsqVxaxQjBQnHQmiI7Vac40t8x7pIb8gLGV6wL7sBTJiPovJ0V7y7oc0Ye" +
-									"rhKh0Rm4skP2z/jHwwZICgGzBvA0rH8xlhUiTvcwDCJ0kc+fh35hNt8srZQM4619" +
-								"FTgB66Xmp4EtVyhpQV+t02g6NzK72oZI0vnAvqhpkxLeLiMCyrI416wHm5Tkukhx" +
-							"QmcL2a6hNOyu0ixX/x2kSFXApEnVrJ+/IxGyfyw8kf4N2IZpW5nEP847lpfj0SZZ" +
-						"Fwrd1mnfnDbYohX2zRptLy2ZUn06Qo9pkG5ntvFEPo9bfZeULtjYzIl6K8gJ2uGZ" + "HQIDAQAB", "12345", "",
+		"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuMPNS1Ufof9EW/M98FNw" +
+			"UAKrwflsqVxaxQjBQnHQmiI7Vac40t8x7pIb8gLGV6wL7sBTJiPovJ0V7y7oc0Ye" +
+			"rhKh0Rm4skP2z/jHwwZICgGzBvA0rH8xlhUiTvcwDCJ0kc+fh35hNt8srZQM4619" +
+			"FTgB66Xmp4EtVyhpQV+t02g6NzK72oZI0vnAvqhpkxLeLiMCyrI416wHm5Tkukhx" +
+			"QmcL2a6hNOyu0ixX/x2kSFXApEnVrJ+/IxGyfyw8kf4N2IZpW5nEP847lpfj0SZZ" +
+			"Fwrd1mnfnDbYohX2zRptLy2ZUn06Qo9pkG5ntvFEPo9bfZeULtjYzIl6K8gJ2uGZ" + "HQIDAQAB", "12345", "",
 		"Vml2YW11cyBmZXJtZtesting123"}
 	expected := []bool{true, true, true, true, false, false, false}
 	for i := 0; i < len(tests); i++ {
