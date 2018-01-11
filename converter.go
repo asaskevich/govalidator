afc18e0aada2cccf994545f29224a5aefc0a62f3
Jason Lam <jasonlam604@gmail.com>
2018-01-10 22:07:25 -0800

    Refactor ToInt (converter.go) to handle all int types and string type and updated InRangeInt (numerics.go) to handle all int types not just type int

--- converter.go
+++ converter.go
@@ -3,6 +3,7 @@ package govalidator
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -31,12 +32,38 @@ func ToFloat(str string) (float64, error) {
 }
 
 // ToInt convert the input string to an integer, or 0 if the input is not an integer.
-func ToInt(str string) (int64, error) {
+func ToIntX(str string) (int64, error) {
 	res, err := strconv.ParseInt(str, 0, 64)
 	if err != nil {
 		res = 0
 	}
 	return res, err
+}
+
+func ToInt(value interface{}) (res int64, err error) {
+	val := reflect.ValueOf(value)
+
+	switch value.(type) {
+	case int, int8, int16, int32, int64:
+		res = val.Int()
+	case uint, uint8, uint16, uint32, uint64:
+		res = int64(val.Uint())
+	case string:
+		if IsInt(val.String()) {
+			res, err = strconv.ParseInt(val.String(), 0, 64)
+			if err != nil {
+				res = 0
+			}
+		} else {
+			err = fmt.Errorf("math: square root of negative number %g", value)
+			res = 0
+		}
+	default:
+		err = fmt.Errorf("math: square root of negative number %g", value)
+		res = 0
+	}
+
+	return
 }
 
 // ToBoolean convert the input string to a boolean.
