77b96c7cf3a64f15b78e1a62a3d61449dfc2a68b
Mohammadjafari80 <mamad.jafari91@gmail.com>
2021-02-04 14:35:44 +0330

    implement firstError test

--- error_test.go
+++ error_test.go
@@ -27,3 +27,29 @@ func TestErrorsToString(t *testing.T) {
 		}
 	}
 }
+
+func TestErrors_firstError(t *testing.T) {
+
+	t.Parallel()
+	customErr := &Error{Name: "Custom Error Name", Err: fmt.Errorf("stdlib error")}
+	customErrWithCustomErrorMessage := &Error{Name: "Custom Error Name 2", Err: fmt.Errorf("Bad stuff happened"), CustomErrorMessageExists: true}
+
+	tests := []struct {
+		param1   Errors
+		expected string
+	}{
+		{Errors{}, ""},
+		{Errors{fmt.Errorf("Error 1")}, "Error 1"},
+		{Errors{fmt.Errorf("Error 1"), fmt.Errorf("Error 2")}, "Error 1"},
+		{Errors{customErr, fmt.Errorf("Error 2")}, "Custom Error Name: stdlib error"},
+		{Errors{fmt.Errorf("Error 123"), customErrWithCustomErrorMessage}, "Error 123"},
+	}
+
+	for _, test := range tests {
+		actual := test.param1.firstError()
+		if actual != test.expected {
+			t.Errorf("Expected Error() to return '%v', got '%v'", test.expected, actual)
+		}
+	}
+
+}
